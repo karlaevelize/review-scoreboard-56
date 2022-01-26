4dcee807fa33c9ec7e3da3225d70f46ddbfacddc
karlaevelize <k.evelize@gmail.com>
2022-01-26 11:08:15 +0100

    Added more commentd to the code

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -5,14 +5,14 @@ export default function Characters() {
 
   const [ characters, setCharacters ] = useState()
 
-//Step 1. Import Axios
-//Step 2. Write an async function to fetch the data
-//Step 3. Make a request with axios
-//Step 4. Import useEffect and call the function
-//Step 5. Console.log the response
-//Step 6. Declare a variable with local state
-//Step 7. Put the data in local state
-//STep 8. Map over the data
+  //Step 1. Import Axios
+  //Step 2. Write an async function to fetch the data
+  //Step 3. Make a request with axios
+  //Step 4. Import useEffect and call the function
+  //Step 5. Console.log the response
+  //Step 6. Declare a variable with local state to hold the data you fetched, ex: const [ characters, setCharacters ] = useState()
+  //Step 7. Put the data in local state using the setter, ex:  setCharacters(response.data.rows)
+  //STep 8. Map over the data
 
   async function getData(){
     const response = await axios.get("https://hp-assessment-api.herokuapp.com/got/characters")
@@ -31,6 +31,7 @@ export default function Characters() {
     <div>
       <h1>Characters list</h1>
       {!characters 
+        //conditional rendering, in case something unexpected happens, like: slow internet, API breaks 
         ? "Loading" 
         : characters.map(char => {
           return (
