efc7be8c4ce9c407d5deb55951d8d24024dc1aff
karlaevelize <k.evelize@gmail.com>
2022-01-26 11:02:51 +0100

    fetching data review

---
+++ src/components/Characters.js
@@ -0,0 +1,45 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
+
+export default function Characters() {
+
+  const [ characters, setCharacters ] = useState()
+
+//Step 1. Import Axios
+//Step 2. Write an async function to fetch the data
+//Step 3. Make a request with axios
+//Step 4. Import useEffect and call the function
+//Step 5. Console.log the response
+//Step 6. Declare a variable with local state
+//Step 7. Put the data in local state
+//STep 8. Map over the data
+
+  async function getData(){
+    const response = await axios.get("https://hp-assessment-api.herokuapp.com/got/characters")
+    console.log("response", response)
+    setCharacters(response.data.rows)
+  }
+  
+  // getData() //this ends in an infinite loop
+  console.log(characters)
+
+  useEffect(() => { //use Effect only runs once, when the page renderes
+    getData()
+  }, [])
+
+  return (
+    <div>
+      <h1>Characters list</h1>
+      {!characters 
+        ? "Loading" 
+        : characters.map(char => {
+          return (
+            <div>
+              <h3>{char.name}</h3>
+              <img style={{width: 200}} src={char.imageUrl} />
+            </div>
+          )
+        })}
+    </div>
+  )
+}
